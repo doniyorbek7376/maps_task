125b9cfb0962ae4e2eca979bdda35291b49fbcdb
doniyorbek7376 <doniyorbek6439@gmail.com>
2020-06-16 10:35:46 +0500

    Initial commit

---
+++ app/src/main/java/uz/doniyorbekurinboyev/avitotech/MapsActivity.kt
@@ -0,0 +1,31 @@
+package uz.doniyorbekurinboyev.avitotech
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.google.android.gms.maps.CameraUpdateFactory
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.MarkerOptions
+import com.google.android.material.floatingactionbutton.FloatingActionButton
+
+class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
+
+    private lateinit var mMap: GoogleMap
+    private lateinit var mFloatingActionButton: FloatingActionButton
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_maps)
+        val mapFragment = supportFragmentManager
+                .findFragmentById(R.id.map) as SupportMapFragment
+        mapFragment.getMapAsync(this)
+        mFloatingActionButton = findViewById(R.id.floating_action_button)
+    }
+
+    override fun onMapReady(googleMap: GoogleMap) {
+        mMap = googleMap
+
+    }
+}
